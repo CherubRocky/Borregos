dae6dff4041f69559c9b90b1eb5f91a18f10fe57
Rocky <zar@ciencias.unam.mx>
2025-11-17 21:47:27 -0600

    Agregadas las consultas 1 y 10.

--- Practica09/SQL/Query.sql
+++ Practica09/SQL/Query.sql
@@ -1,5 +1,8 @@
 
 -- Consulta 1
+SELECT Nombre, Paterno, Materno, CodigoEntrenador, NombreUsuario, Nivel, Equipo FROM
+Persona INNER JOIN Participante ON Persona.IdPersona = Participante.IdPersona
+INNER JOIN Cuenta ON Participante.IdPersona = Cuenta.IdPersona;
 -- Consulta 2
 -- Consulta 3
 -- Consulta 4
@@ -17,3 +20,28 @@ WHERE Pokemon.Shiny = TRUE AND EXTRACT(HOUR FROM Capturar.FechaYHora) BETWEEN 14
 -- Consulta 8
 -- Consulta 9
 -- Consulta 10
+WITH
+DistTotal AS (
+   SELECT IdPersona, SUM(Distancia) AS DistanciaTotal
+   FROM Recorrer
+   GROUP BY IdPersona
+),
+PromDistancia AS (
+   SELECT
+       AVG(DistanciaTotal) AS Promedio
+   FROM DistTotal
+),
+TotalCapturados AS (
+   SELECT
+       IdPersona,
+       COUNT(IdPersona) AS Capturados
+       FROM Capturar INNER JOIN Pokemon ON Capturar.IdPokemon = Pokemon.IdPokemon
+       WHERE Shiny = true
+       GROUP BY IdPersona
+)
+
+SELECT Nombre, Paterno, Materno, Facultad
+FROM Persona INNER JOIN Participante ON Persona.IdPersona = Participante.IdPersona
+INNER JOIN TotalCapturados ON TotalCapturados.IdPersona = Participante.IdPersona
+INNER JOIN DistTotal ON DistTotal.IdPersona = Participante.IdPersona
+WHERE TotalCapturados.Capturados > 5 AND DistTotal.DistanciaTotal > (SELECT Promedio FROM PromDistancia);
