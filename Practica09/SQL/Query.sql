704a350159fe2d1177471d16ae392da27769dd39
IsaacEscobar09 <isaacesc@ciencias.unam.mx>
2025-11-19 17:27:24 -0600

    Consultas 3 y 8 funcionando, y corrección de la consulta 6

--- Practica09/SQL/Query.sql
+++ Practica09/SQL/Query.sql
@@ -2,6 +2,10 @@
 -- Consulta 1
 -- Consulta 2
 -- Consulta 3
+SELECT *
+FROM Pokemon
+WHERE especie LIKE '%chu%';
+
 -- Consulta 4
 SELECT DISTINCT pa.IdPersona, per.Nombre, per.Paterno, per.Materno
 FROM Participar pa
@@ -9,6 +13,7 @@ INNER JOIN Persona per ON pa.IdPersona = per.IdPersona
 INNER JOIN CapturaShiny cs ON pa.IdTorneo = cs.IdTorneo
 WHERE pa.IdPersona NOT IN ( SELECT pa2.IdPersona FROM Participar pa2 INNER JOIN Recorrido r ON pa2.IdTorneo = r.IdTorneo
 );
+
 -- Consulta 5
 SELECT Participante.IdPersona, Persona.Nombre, Participante.NoCuenta, SUM(Recorrer.Distancia) AS DistanciaTotal FROM Recorrer 
 INNER JOIN Participante ON Recorrer.IdPersona = Participante.IdPersona
@@ -17,10 +22,9 @@ GROUP BY Participante.IdPersona, Persona.Nombre, Participante.NoCuenta;
 
 -- Consulta 6
 SELECT * FROM Pokemon INNER JOIN Capturar ON Pokemon.IdPokemon = Capturar.IdPokemon
-WHERE Pokemon.Shiny = TRUE AND EXTRACT(HOUR FROM Capturar.FechaYHora) BETWEEN 14 AND 18;
+WHERE Pokemon.Shiny = TRUE AND CAST(Capturar.FechaYHora as TIME) BETWEEN '14:00:00' AND '18:00:00';
 
 -- Consulta 7
-
 SELECT per.Nombre, per.Paterno, per.Materno, v.IdPersona AS IdVendedor, a.IdAlimento, a.Nombre AS NombreAlimento, a.PrecioSinIVA AS PrecioSinIVA, (a.PrecioSinIVA * 1.16) AS PrecioConIVA
 FROM Vendedor v INNER JOIN Empleado e ON v.IdPersona = e.IdPersona
 INNER JOIN Persona per ON e.IdPersona = per.IdPersona
@@ -29,6 +33,19 @@ INNER JOIN Venta ve ON pa.IdPersona = ve.IdPersonaV
 INNER JOIN Registrar r ON ve.IdVenta = r.IdVenta
 INNER JOIN Alimento a ON r.IdAlimento = a.IdAlimento
 ORDER BY per.Paterno, per.Nombre, a.Nombre;
+
 -- Consulta 8
+SELECT 
+    Facultad, 
+    COUNT(DISTINCT Participar.IdPersona) AS TotalParticipantes
+FROM 
+    Participar
+JOIN 
+    Participante ON Participar.IdPersona = Participante.IdPersona
+GROUP BY 
+    Facultad
+HAVING 
+    COUNT(DISTINCT Participar.IdPersona) > 5;
+
 -- Consulta 9
 -- Consulta 10
