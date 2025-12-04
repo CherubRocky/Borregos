2861fd9eccce9e802d91c6ad90e1ec85e9f6f497
Rocky <zar@ciencias.unam.mx>
2025-12-03 20:32:21 -0600

    Ganar poblada, teóricamente ya está terminada.

---
+++ ProyectoFinal_Borregos/Ganar.sql
@@ -0,0 +1,10 @@
+INSERT INTO Ganar (IdPersona, IdTorneo) VALUES (4013, 1);
+INSERT INTO Ganar (IdPersona, IdTorneo) VALUES (4173, 2);
+INSERT INTO Ganar (IdPersona, IdTorneo) VALUES (4425, 3);
+INSERT INTO Ganar (IdPersona, IdTorneo) VALUES (4014, 4);
+INSERT INTO Ganar (IdPersona, IdTorneo) VALUES (4170, 5);
+INSERT INTO Ganar (IdPersona, IdTorneo) VALUES (4427, 6);
+INSERT INTO Ganar (IdPersona, IdTorneo) VALUES (2, 7);
+INSERT INTO Ganar (IdPersona, IdTorneo) VALUES (4151, 8);
+INSERT INTO Ganar (IdPersona, IdTorneo) VALUES (4434, 9);
+
